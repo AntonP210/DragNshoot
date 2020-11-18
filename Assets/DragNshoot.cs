7c73ab2a9a3bf8033fb8788aa722c4a077ce2ba1
Anton <antonpas.dev@gmail.com>
2020-11-18 22:08:22 +0200

    Initial Commit

---
+++ Assets/DragNshoot.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragNshoot : MonoBehaviour
+{
+    public float power = 10f;
+    public Rigidbody2D rigidBody;
+
+    public Vector2 minPower;
+    public Vector2 maxPower;
+
+    TrajectoryLine trajectoryLine;
+
+    Camera cam;
+
+    Vector2 force;
+    Vector3 startPoint;
+    Vector3 endPoint;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        trajectoryLine = GetComponent<TrajectoryLine>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            startPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            startPoint.z = 15;
+        }
+        if (Input.GetMouseButton(0)) {
+            Vector3 currentPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            startPoint.z = 15;
+            trajectoryLine.RenderLine(startPoint, currentPoint);
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            startPoint.z = 15;
+
+            force = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x)
+            , Mathf.Clamp(startPoint.y - endPoint.y, minPower.y, maxPower.y));
+
+            rigidBody.AddForce(force * power, ForceMode2D.Impulse);
+
+            trajectoryLine.EndLine();
+        }
+    }
+}
